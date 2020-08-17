5c65626b0485e501ebf8d5b7f0fc270ea3260f07
Jarwo Template <67773360+JarwoTemplate@users.noreply.github.com>
2020-08-17 09:02:26 +0700

    Update 1.js

--- lkj/1.js
+++ lkj/1.js
@@ -3,6 +3,7 @@ var knil=[
 "jarwo-lirge.blogspot.com","pertanyaansekolah.blogspot.com",
 "kodejarwo.com","6508876407973009500",
 "tukang-listrik.com","2722220767065175407",
+"jarwo-agroekotekno","agroekotekno.com","9000838746591063808",
 // GA #2
 "haxerise.xyz","7784785690439060651",
 "kabarpns.com","4758397360279400455",
