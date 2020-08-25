a75e2102cbe3c28224513ec5c72cd96f5b9ac01a
Jarwo Template <jarwotemplate@gmail.com>
2020-08-25 15:23:24 +0700

    Update a.js

--- template/webstore/lkj/a.js
+++ template/webstore/lkj/a.js
@@ -1,4 +1,4 @@
 // “Ingatlah, laknat Allah (ditimpakan) atas orang-orang yang zalim” (QS. Hud: 18) //
 var knil=[
-"jarwo-webstore.blogspot.com","8307923606017148913"
+"jarwo-webstore.blogspot.com","8307923606017148913","kodejarwo.com"
 ];
