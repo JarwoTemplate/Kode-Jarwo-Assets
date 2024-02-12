1d9bbec9a3b0a01e0918c56c8e79e38792bd82aa
Jarwo Template <67773360+JarwoTemplate@users.noreply.github.com>
2024-02-12 13:34:49 +0700

    Update 1.js
    Penambahan primbonjk

--- template/lirge/lkj/1.js
+++ template/lirge/lkj/1.js
@@ -2,5 +2,5 @@
 // No ID //
 var knil=[
 "jarwo-lirge.blogspot.com","pertanyaansekolah.blogspot.com","saladinez.kodejarwo.com","6508876407973009500","jarwo-agroekotekno","agroekotekno.com","9000838746591063808",
-"2259469876522752859","tukang-listrik.com","2722220767065175407","haxerise.xyz","7784785690439060651","kabarpns.com","4758397360279400455","ijamu.com","5116522222985387892","talmaprint.com","6720558180632229876","320105936692657573","jarwo-tsj.blogspot.com","amertapedia.com","turnesia.com","6216100933349951745","anjelotour.blogspot.com","anjelotour.com"
+"2259469876522752859","tukang-listrik.com","2722220767065175407","haxerise.xyz","7784785690439060651","kabarpns.com","4758397360279400455","ijamu.com","5116522222985387892","talmaprint.com","6720558180632229876","320105936692657573","jarwo-tsj.blogspot.com","amertapedia.com","turnesia.com","6216100933349951745","anjelotour.blogspot.com","anjelotour.com","primbonjk.com"
 ];
